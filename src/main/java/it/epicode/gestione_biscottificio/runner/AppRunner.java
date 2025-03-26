6c85a04e2c4efe0275063ccffc70d66a0fb54166
Michele Mandanici <michimandanici@tim.it>
2025-03-26 23:15:31 +0100

    .

--- src/main/java/it/epicode/gestione_biscottificio/runner/AppRunner.java
+++ src/main/java/it/epicode/gestione_biscottificio/runner/AppRunner.java
@@ -35,16 +35,11 @@ public class AppRunner implements ApplicationRunner {
 
             // Prodotti per tipici Castrensi
             productService.saveProduct(createProduct("Biscotti Castriciani", "https://res.cloudinary.com/dn9wsjpqo/image/upload/v1741441716/Products/2.webp.webp", 3.5, "Biscotti Castriciani morbidi", tipiciCastrensi));
-            productService.saveProduct(createProduct("Occhi di Bue", "https://image.url/2.jpg", 2.8, "Occhi di Bue con marmellata", tipiciCastrensi));
-            productService.saveProduct(createProduct("Biscotti Castriciani", "https://image.url/3.jpg", 2.5, "Biscotti Castriciani morbidi", tipiciCastrensi));
 
             // Prodotti per pasticceria
-            productService.saveProduct(createProduct("Torta di Mele", "https://image.url/4.jpg", 8.0, "Torta di mele classica", pasticceria));
-            productService.saveProduct(createProduct("Tiramisù", "https://image.url/5.jpg", 10.0, "Tiramisù della casa", pasticceria));
             productService.saveProduct(createProduct("Cheesecake", "https://image.url/6.jpg", 9.0, "Cheesecake ai frutti di bosco", pasticceria));
 
             // Prodotti per classici
-            productService.saveProduct(createProduct("Crostata di Albicocche", "https://image.url/7.jpg", 7.0, "Crostata con marmellata di albicocche", classici));
             productService.saveProduct(createProduct("Crostata di Nutella", "https://image.url/8.jpg", 7.5, "Crostata con crema di nocciole", classici));
 
         }
