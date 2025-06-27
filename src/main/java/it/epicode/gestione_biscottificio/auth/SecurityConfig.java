d4f11f8961ca157e7e1cd551bffbcd8251758cb7
Michele Mandanici <michimandanici@tim.it>
2025-06-27 12:28:47 +0200

    dominio

--- src/main/java/it/epicode/gestione_biscottificio/auth/SecurityConfig.java
+++ src/main/java/it/epicode/gestione_biscottificio/auth/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://capstone-project-front-end-hazel.vercel.app"
+                "https://demo-biscottificio.vercel.app"
         ));
         corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfig.setAllowedHeaders(List.of("*"));
